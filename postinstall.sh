5b02ae23d93a2e3fdee8574ba685ca2e862471d6
joel <joel@redballoonsecurity.com>
2022-01-05 11:55:40 -0500

    move bundle to /Library

--- postinstall.sh
+++ postinstall.sh
@@ -6,5 +6,5 @@
 # Created by Zydeco on 28/5/2010.
 # Copyright 2010 namedfork.net. All rights reserved.
 
-chown root:wheel /System/Library/Filesystems/fusefs_hfs.fs
-ln -fs /System/Library/Filesystems/fusefs_hfs.fs/Contents/Resources/fusefs_hfs /sbin/mount_fusefs_hfs
+chown root:wheel /Library/Filesystems/fusefs_hfs.fs
+ln -fs /Library/Filesystems/fusefs_hfs.fs/Contents/MacOS/fusefs_hfs /usr/local/mount_fusefs_hfs
